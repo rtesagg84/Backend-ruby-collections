15704e2162dd11bc3229816ca8aa1c5dcac023d5
rtesagg84 <rtesagg84@gmail.com>
2021-10-26 01:27:16 -0400

    code indentation done

--- sorted_words.rb
+++ sorted_words.rb
@@ -1,12 +1,12 @@
 array =[]
 loop do
-puts "enter  words as you want (e to exit) :"
-answer = gets.chomp
-array.push(answer)
-puts "#{array}"
- if answer == "e"
-    sort_value = array.sort
-    puts "#{sort_value}"
+     puts "enter  words as you want (e to exit) :"
+     answer = gets.chomp
+   if answer != "e"
+      array.push(answer)
+   else
+      sort_value = array.sort
+      puts " this is the sorted words  in Array format : #{sort_value}"
     break   
- end
+  end
 end
